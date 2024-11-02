9a1ad3cab669c00401b5ae36cfb25c84626aca12
Nils Knappmeier <npm@knappi.org>
2024-11-02 23:24:22 +0100

    try browsers in docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,33 +5,42 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./src",
-  testMatch: '*.spec.ts',
+  testMatch: "*.spec.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: "http://127.0.0.1:5173",
     trace: "on-first-retry",
   },
   expect: {
-    timeout: 1000
+    timeout: 1000,
   },
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        connectOptions: {
+          wsEndpoint: "ws://localhost:3000/",
+        },
+      },
     },
-
     {
       name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      use: {
+        ...devices["Desktop Firefox"],
+        connectOptions: {
+          wsEndpoint: "ws://localhost:3000/",
+        },
+      },
     },
   ],
   webServer: {
-    command: 'npm run dev:server -- --port 5173',
-    url: 'http://localhost:5173',
+    command: "npm run dev:server -- --port 5173",
+    url: "http://localhost:5173",
     stdout: "pipe",
     timeout: 10000,
     reuseExistingServer: !process.env.CI,
