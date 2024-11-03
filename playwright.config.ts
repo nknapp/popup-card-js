40cc0fd22c7b7b55c2e88a5860edfcd5896d8d52
Nils Knappmeier <npm@knappi.org>
2024-11-03 19:34:29 +0100

    use host.docker.internal to access dev-server from e2e-test

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "http://host.docker.internal:5173",
     trace: "on-first-retry",
   },
   expect: {
@@ -30,7 +30,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev:server -- --port 5173",
+    command: "npm run dev:server -- --port 5173 --host 0.0.0.0",
     url: "http://localhost:5173",
     stdout: "pipe",
     timeout: 10000,
--- src/manual-testing/e2e-helper.ts
+++ src/manual-testing/e2e-helper.ts
@@ -17,5 +17,5 @@ function getAssociatedUrl(url: string): string {
   const baseUrl = new URL("..", import.meta.url).href;
   const relativePath = "./" + url.slice(baseUrl.length);
   const associatedModule = relativePath.replace(/\.spec\.ts$/, ".ts");
-  return `http://localhost:5173?test=${encodeURIComponent(associatedModule)}&playwright=true`;
+  return `/?test=${encodeURIComponent(associatedModule)}&playwright=true`;
 }
