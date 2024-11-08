7f6482fed1e61edef83073bcceffd54086d2a9f1
Nils Knappmeier <npm@knappi.org>
2024-11-08 19:08:54 +0100

    fix examples link on home-page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "popup-card-js",
+  base: "/popup-card-js",
   integrations: [
     starlight({
       title: "popup-card-js",
